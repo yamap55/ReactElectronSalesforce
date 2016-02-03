8158ed8878a4eb34fb4c249322a4f89dd0bed59a
shinji_japan <shinji.japan@live.jp>
2016-02-04 00:07:35 +0900

    first commit

---
+++ client/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import Root from './Root.jsx'
+
+let container = document.querySelector("#container")
+
+ReactDOM.render(<Root />, container)
